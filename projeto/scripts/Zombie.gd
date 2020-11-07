646092e93b7a724259b24af8f3251c6dfc8fcca2
lagc <lagc@cin.ufpe.br>
2020-11-07 10:08:51 -0300

    starting main scene

--- projeto/scripts/Zombie.gd
+++ projeto/scripts/Zombie.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 
 export (float) var speed = 100.0
 
+var zombie = get_tree().get_root().get_node("Zombie")
 var target = Vector2(0,0)
-var vector_speed: Vector2 = Vector2(0,0)
 
 
 func _physics_process(delta):
 	# TODO: fix the get_pos()
-	vector_speed = speed * (target - get_pos()).normalized()
+	zombie.move_toward(zombie.get_pos(), target, speed)
