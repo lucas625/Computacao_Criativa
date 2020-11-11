c4d958060330eb905735a04e9a1551fa6f224103
Natanael Neto <natanael.eneas2018@hotmail.com>
2020-11-11 01:38:43 -0300

    C1 - Adding reload/restart system. Restarts the game pressing r-key

--- projeto/scenes/Pause.gd
+++ projeto/scenes/Pause.gd
@@ -1,7 +1,10 @@
 extends Control
 
 func _input(event):
-	if event .is_action_pressed("pause"):
+	if event.is_action_pressed("pause"):
 		var pause_state = not get_tree().paused
 		get_tree().paused = pause_state
 		visible = pause_state
+	if event.is_action_pressed("restart"):
+		get_tree().paused = false
+		get_tree().reload_current_scene()
