fb81b0889ec1eb16a43ed1fdcbec1bd5e35da2f1
Natanael Neto <natanael.eneas2018@hotmail.com>
2020-11-11 01:55:42 -0300

    C2 - Adding hit system. Currently, if a zombia hit the player, the game restarts

--- projeto/scripts/Zombie.gd
+++ projeto/scripts/Zombie.gd
@@ -11,3 +11,8 @@ func _physics_process(delta):
 	var direction = (target - self.position).normalized()
 	var velocity = direction * speed
 	move_and_slide(velocity)
+
+
+
+func _on_Area2D_body_entered(body):
+	get_tree().reload_current_scene() # Replace with function body.
