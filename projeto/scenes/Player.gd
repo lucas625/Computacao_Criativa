f78490b75bb58b176c785ca593bc3f8774cf107a
Thiago Bastos <tfb@cin.ufpe.br>
2020-11-18 05:23:17 -0300

    player can now type

---
+++ projeto/scenes/Player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var typing = ""
+
+onready var label = $Label
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.scancode >= 65 and event.scancode <= 90: #a to z
+			typing = typing + char(event.scancode)
+			print (event.scancode)
+			print (char(event.scancode))
+			label.text = typing
+		elif typing.length() > 0 and event.scancode == 16777220: #backspace
+			typing = typing.substr(0, typing.length()-1)
+			label.text = typing
+	pass
