5869db0686b99daa0a2ffd29a52ade8e892f9bc5
Natanael Neto <natanael.eneas2018@hotmail.com>
2020-11-23 22:34:47 -0300

    C2 - Pause and Restart System

--- projeto/scripts/Pause.gd
+++ projeto/scripts/Pause.gd
@@ -8,3 +8,21 @@ func _input(event):
 	if event.is_action_pressed("restart"):
 		get_tree().paused = false
 		get_tree().reload_current_scene()
+
+
+func _on_PauseBtn_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		var pause_state = not get_tree().paused
+		get_tree().paused = pause_state
+		visible = pause_state
+
+func _on_btnrestart_button_down():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_btncontinue_button_down():
+	var pause_state = not get_tree().paused
+	get_tree().paused = pause_state
+	visible = pause_state
